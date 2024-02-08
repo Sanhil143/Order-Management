c784d2c51a12a8019f35c683d5368d29cc6501fc
Sanhil143 <sanhilrai143@gmail.com>
2024-02-09 02:02:08 +0530

    update env

--- src/controllers/commnController.js
+++ src/controllers/commnController.js
@@ -1,98 +1,127 @@
-const customerModel = require('../model/customerModel')
-const productModel = require('../model/productModel')
-const orderModel = require('../model/orderModel')
-const nodemailer = require('nodemailer')
-
+const customerModel = require("../models/customerModel");
+const productModel = require("../models/productModel");
+const orderModel = require("../models/orderModel");
+const nodemailer = require("nodemailer");
 
 exports.createUser = async (req, res) => {
-      try {
-            let data = req.body
-            let { name, email, password, phone } = data
+  try {
+    let data = req.body;
+    let { name, email, password, phone } = data;
 
-            let verify = await customerModel.findOne({ $or: [{ email: email }, { phone: phone }] })
-            if (verify) {
-                  return res.status(400).send({ status: false, message: "email is already exist! please enter another email address  and phone" })
-            }
+    let verify = await customerModel.findOne({
+      $or: [{ email: email }, { phone: phone }],
+    });
+    if (verify) {
+      return res.status(400).send({
+        status: false,
+        message:
+          "email is already exist! please enter another email address  and phone",
+      });
+    }
 
-            let transport = nodemailer.createTransport(
-                  {
-                        service: 'gmail',
-                        auth: { user: 'rahulsaran820@gmail.com', pass: 'kudupqdaegzwpv' }
-                  }
-            )
+    let transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: { user: "rahulsaran820@gmail.com", pass: "kudupqdaegzwpv" },
+    });
 
-            let mailOptions = {
-                  from: 'shyamgupta0214@gmail.com',
-                  to: email,
-                  subject: `"Hello"${name}`,
-                  text: ` hii ${name} your account successfully created`
-            }
+    let mailOptions = {
+      from: "shyamgupta0214@gmail.com",
+      to: email,
+      subject: `"Hello"${name}`,
+      text: ` hii ${name} your account successfully created`,
+    };
 
-            transport.sendMail(mailOptions, function (err, info) {
-                  if (err) return console.log(err.message)
-                  if (info) return consol.log('Email Sent' + info.response)
+    transport.sendMail(mailOptions, function (err, info) {
+      if (err) return console.log(err.message);
+      if (info) return consol.log("Email Sent" + info.response);
+    });
 
-            })
-
-            let createUser = await customerModel.create(data)
-            return res.status(201).send({ status: true, message: "create user", data: createUser })
-      } catch (err) {
-            return res.status(500).send({ status: false, message: err.message })
-      }
-}
+    let createUser = await customerModel.create(data);
+    return res
+      .status(201)
+      .send({ status: true, message: "create user", data: createUser });
+  } catch (err) {
+    return res.status(500).send({ status: false, message: err.message });
+  }
+};
 
 exports.createProduct = async (req, res) => {
-      try {
-            let data = req.body
+  try {
+    let data = req.body;
 
-            let createProduct = await productModel.create(data)
-            res.status(201).send({ status: true, data: createProduct })
-
-      } catch (err) {
-            return res.status(500).send({ status: false, message: err.message })
-      }
-}
+    let createProduct = await productModel.create(data);
+    res.status(201).send({ status: true, data: createProduct });
+  } catch (err) {
+    return res.status(500).send({ status: false, message: err.message });
+  }
+};
 
 exports.createOrder = async (req, res) => {
-      try {
-            let data = req.body
-            let { userId, demandQuantity, productId } = data
+  try {
+    let data = req.body;
+    let { userId, demandQuantity, productId } = data;
 
-            let checkUser = await customerModel.findById({ _id: userId })
-            if (!checkUser) return res.status(404).send({ status: false, message: " user id is not found" })
-            let checkProduct = await productModel.findOne({ _id: productId, isDeleted: false })
-            if (!checkProduct) return res.status(404).send({ status: false, message: "product id is not found" })
+    let checkUser = await customerModel.findById({ _id: userId });
+    if (!checkUser)
+      return res
+        .status(404)
+        .send({ status: false, message: " user id is not found" });
+    let checkProduct = await productModel.findOne({
+      _id: productId,
+      isDeleted: false,
+    });
+    if (!checkProduct)
+      return res
+        .status(404)
+        .send({ status: false, message: "product id is not found" });
 
-            let userObj = {}
-            if (checkUser.order > 10) { userObj.category = "gold" }
-            if (checkUser.order > 20) { userObj.category = "platinum" }
+    let userObj = {};
+    if (checkUser.order > 10) {
+      userObj.category = "gold";
+    }
+    if (checkUser.order > 20) {
+      userObj.category = "platinum";
+    }
 
-            if (demandQuantity > checkProduct.quantity) return res.status(400).send({ status: false, message: " quantity is greater then product quantity" })
+    if (demandQuantity > checkProduct.quantity)
+      return res.status(400).send({
+        status: false,
+        message: " quantity is greater then product quantity",
+      });
 
+    let amount = checkProduct.price * demandQuantity;
 
-            let amount = checkProduct.price * demandQuantity
+    if (checkUser.category == "gold") {
+      amount = amount * 0.1;
+    }
+    if (checkUser.category == "platinum") {
+      amount = amount * 0.2;
+    }
 
-            if (checkUser.category == "gold") { amount = amount * 0.1 }
-            if (checkUser.category == "platinum") { amount = amount * 0.2 }
+    if (checkUser.balance < amount) {
+      return res.status(400).send({
+        status: false,
+        message: `insufficient balance you must have ${
+          amount - checkUser.balance
+        }₹ extra`,
+      });
+    }
 
+    let createOrder = await orderModel.create(data);
+    await productModel.findByIdAndUpdate(
+      { _id: productId },
+      { $inc: { quantity: -demandQuantity } }
+    );
 
-            if (checkUser.balance < amount) {
-                  return res.status(400).send({ status: false, message: `insufficient balance you must have ${amount - checkUser.balance}₹ extra` })
-            }
+    await customerModel.findByIdAndUpdate(
+      { _id: userId },
+      { userObj, $inc: { balance: -amount }, $inc: { order: 1 } }
+    );
 
-
-
-            let createOrder = await orderModel.create(data)
-            await productModel.findByIdAndUpdate({ _id: productId },
-                  { $inc: { quantity: -demandQuantity } })
-
-            await customerModel.findByIdAndUpdate({ _id: userId },
-                  { userObj, $inc: { balance: -amount }, $inc: { order: 1 } })
-
-            return res.status(201).send({ status: true, message: "create order", data: createOrder })
-
-      } catch (err) {
-            return res.status(500).send({ status: false, message: err.message })
-      }
-
-}
+    return res
+      .status(201)
+      .send({ status: true, message: "create order", data: createOrder });
+  } catch (err) {
+    return res.status(500).send({ status: false, message: err.message });
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const dotenv = require('dotenv');
 const router = require('./routers/router')
 mongoose.set('strictQuery', false)
 
 const app = express()
 app.use(express.json())
+dotenv.config();
 
-
-mongoose.connect("mongodb+srv://sanhil143:raisahab12345@sanhildb.kk3knyj.mongodb.net/Order-Management")
+mongoose.connect(process.env.MY_DB_URL)
 .then(() => console.log("mongo DB is connected"))
 .catch((err) => console.error(err))
 
